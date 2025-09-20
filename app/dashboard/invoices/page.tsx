fecefeaddcbbe67c46b7ffcfb26439c7eccf7675
tctc <1325109177@qq.com>
2025-09-20 15:53:59 +0800

    添加 Prettier 配置文件和忽略文件，更新 package.json 以支持格式化和 lint 命令，修改 VSCode 设置以集成 Prettier 和 Tailwind CSS，优化发票页面代码格式。

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,24 +1,23 @@
-import Pagination from '@/app/ui/invoices/pagination';
-import Search from '@/app/ui/search';
-import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
-import { fetchInvoicesPages } from '@/app/lib/data';
+import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
+import Table from "@/app/ui/invoices/table";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { lusitana } from "@/app/ui/fonts";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import { fetchInvoicesPages } from "@/app/lib/data";
 export default async function Page(props: {
-    searchParams?: Promise<{
-      query?: string;
-      page?: string;
-      size?: string
-    }>;
-  }) {
-    
-    const searchParams = await props.searchParams;
-    const query = searchParams?.query || '';
-    const size = searchParams?.size || '';
-    const currentPage = Number(searchParams?.page) || 1;
-    const totalPages = await fetchInvoicesPages(query,size)
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+    size?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const size = searchParams?.size || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query, size);
 
   return (
     <div className="w-full">
@@ -29,7 +28,7 @@ export default async function Page(props: {
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} size={size} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
